277a96386344cd1df72a6e071633ca161d456b9a
debasiskar-devel-007 <debasiskar007@gmail.com>
2016-08-30 19:17:32 +0530

    30th august

--- app/pages/login/login.ts
+++ app/pages/login/login.ts
@@ -4,6 +4,9 @@ import {FORM_DIRECTIVES, REACTIVE_FORM_DIRECTIVES} from '@angular/forms';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {ProfilePage} from '../profile/profile'
 import {Control} from "@angular/common";
+import {Http, Headers} from "@angular/http";
+import { Storage, LocalStorage } from 'ionic-angular';
+//import * as $ from "jquery";
 
 /*
   Generated class for the LoginPage page.
@@ -16,13 +19,19 @@ import {Control} from "@angular/common";
   directives: [FORM_DIRECTIVES, REACTIVE_FORM_DIRECTIVES]
 })
 export class LoginPage {
+  public _loginUrl:string = "http://torkq.com/login";
   private loginForm:FormGroup;
+  public verifyemail=true;
+  public verifylogin=true;
+  private data;
+  private data1;
   private nav:NavController;
+  private local:LocalStorage;
   profilePage=ProfilePage;
 
-  constructor(fb: FormBuilder,public navCtrl: NavController) {
+  constructor(fb: FormBuilder,public navCtrl: NavController,private _http: Http) {
     this.loginForm = fb.group({
-      email: ["", Validators.required,emailValidator],
+      email: ["", Validators.required],
       password: ["", Validators.required]
     });
   }
@@ -30,18 +39,59 @@ export class LoginPage {
     //console.log(this.loginForm.value);
     console.log(99);
     console.log(event);
+    console.log(event.email);
+    if(!validateEmail(event.email) && event.email !=''){
+      this.verifyemail=false;
+      return;
+    }
     if(this.loginForm.valid){
-      this.navCtrl.push(ProfilePage);
+      var body = 'email='+event.email+'&password='+event.password;
+      var headers = new Headers();
+      headers.append('Content-Type', 'application/x-www-form-urlencoded');
+
+      var link = 'http://torqkd.com/user/ajs1/ioniclogin';
+      //var data = $.param({email: event.email,password:event.password});
+      //var data = JSON.stringify({email: event.email,password:event.password});
+      var data = {email: event.email,password:event.password};
+
+      this._http.post(link, data)
+          .subscribe(data => {
+            // /this.data1.response = data.json();
+            console.log(data);
+            if(data.json()==null){
+              this.verifylogin=false;
+
+              return;
+            }
+            else{
+
+              this.verifylogin=true;
+              this.local = new Storage(LocalStorage);
+              this.local.set('userinfo', JSON.stringify(data.json()));
+              this.local.get('userinfo').then((value) => {
+                //alert(value)
+                var dt = JSON.parse(value);
+                console.log("Local Storage value:", dt.id);
+                console.log("Local Storage value id:", value);
+                console.log("Local Storage value fname:", JSON.stringify(dt));
+              })
+              this.navCtrl.push(ProfilePage);
+
+            }
+          }, error => {
+            console.log("Oooops!");
+          });
+
+
+      //this.navCtrl.push(ProfilePage);
     }
   }
 
 }
 
-function emailValidator(control) {
-  var EMAIL_REGEXP = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
-
-  if (!EMAIL_REGEXP.test(control.value)) {
-    return {invalidEmail: true};
-  }
+function validateEmail(email) {
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  //console.log('vali email called');
+  return re.test(email);
 }
 
